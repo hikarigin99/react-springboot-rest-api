47600f0e8c0046a17828d18968576b248926da88
hikarigin99 <hikarigin99@gmail.com>
2022-12-09 05:22:55 +0900

    [ADD] Order /api/v1/orders post 구현

---
+++ backend/STARBUCKS/src/main/java/com/example/gccoffee/order/controller/OrderRestController.java
@@ -0,0 +1,27 @@
+package com.example.gccoffee.order.controller;
+
+import com.example.gccoffee.order.dto.CreateOrderRequest;
+import com.example.gccoffee.order.vo.Order;
+import com.example.gccoffee.order.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderRestController {
+
+    private final Logger logger = LoggerFactory.getLogger("OrderRestController");
+    private final OrderService orderService;
+
+    public OrderRestController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/api/v1/orders")
+    public Order createOrder(@RequestBody CreateOrderRequest orderRequest) {
+        logger.info("{} {}", orderRequest.getPhoneNumber(), orderRequest.getReceiveType());
+        return orderService.createOrder(orderRequest.getPhoneNumber(), orderRequest.getOrderStatus(), orderRequest.getReceiveType(), orderRequest.getOrderItems());
+    }
+}
